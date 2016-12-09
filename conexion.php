190284772b24476ed250c17d241dbf216ef0bdc2
AlberthoReyes <alberto-reyes@live.com.mx>
2016-12-08 21:53:50 -0600

    modificacion origen

---
+++ conexion.php
@@ -0,0 +1,8 @@
+<?php
+$server="server58.hostinger.mx";
+$usuario="u284037039_alber";
+$password="perroni1.";
+$baseDatos="u284037039_dif";
+$conexion= mysql_connect($server, $usuario, $password, $baseDatos) or die("Error al conectarse al servidor de base de datos".mysql_error());
+?>
+
